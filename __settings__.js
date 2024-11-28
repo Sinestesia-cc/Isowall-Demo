cb5bc802fd749050a0ad9d08eb83c55c58c13b5f
JulioJulioso <julio-ro@hotmail.cl>
2024-11-28 09:59:27 -0300

    0.1

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2122835.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 205635181, 205635183, 205635187, 205635174, 205635176, 205635188, 205635189, 205635190, 205663995, 205635191, 205635180, 205635192, 205635179, 205635172, 205635166, 205635162, 205635416, 205635413, 205635130, 205664525, 205687950, 205693041, 205693518, 205693860 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
